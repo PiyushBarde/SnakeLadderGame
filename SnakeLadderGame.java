3a3dfe99bb7717c0badfe42d29d3c9cadd0aa9b1
PiyushBarde <piyushbarde1@gmail.com>
2021-12-10 23:25:46 +0530

    ADDED: dice roll UC2

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -2,5 +2,7 @@ public class SnakeLadderGame{
 	public static void main(String[] args){
 	int position = 0;
 	System.out.println("Position is :" + position);
-	}
+	double roll = (Math.floor(Math.random() * 10) % 6) + 1;
+        System.out.println((int)roll);
+		}
 }
