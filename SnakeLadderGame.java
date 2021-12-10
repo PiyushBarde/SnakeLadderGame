a2c7afb55154bff88b40bf036a650eb0e8a8242a
PiyushBarde <piyushbarde1@gmail.com>
2021-12-10 23:45:06 +0530

    ADDED:player reaches winning position 100

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -1,8 +1,30 @@
 public class SnakeLadderGame{
-	public static void main(String[] args){
-	int position = 0;
-	System.out.println("Position is :" + position);
-	double roll = (Math.floor(Math.random() * 10) % 6) + 1;
-        System.out.println((int)roll);
-		}
+public static void main(String[] args) {
+        int pos1 = 0;
+        while (pos1 < 100) {
+            double dice1 = (Math.floor(Math.random() * 10) % 6) + 1;
+            System.out.println((int) dice1);
+            double cond1 = Math.floor(Math.random() * 10) % 3;
+            if (pos1 == 100) {
+                break;
+            } else if ((int) cond1 == 0)
+                System.out.println("No Play || current position of player :" + pos1);
+            else if ((int) cond1 == 1) {
+                    pos1 = pos1 + (int) dice1;
+                    System.out.println("Ladder || current position of player :" + pos1);
+            }
+					else if ((int) cond1 == 2) {
+                if (pos1 == 0) {
+                    System.out.println("Roll again || current position of player :" + pos1);
+               		 }
+						}
+					else if (pos1 == (int) dice1 || pos1 <= (int) dice1) {
+                    pos1 = 0;
+                }
+					else {
+                    pos1 = pos1 - (int) dice1;
+                    System.out.println("Snake || current position of player :" + pos1);
+                }
+            }
+	}
 }
